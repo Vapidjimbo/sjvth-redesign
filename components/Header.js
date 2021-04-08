87399b0eba49f758a4161e7db0c0fc465a205963
Vapidjimbo <56098158+Vapidjimbo@users.noreply.github.com>
2021-04-07 19:36:22 -0700

    Inital Commit

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    minWidth: '100%',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  toolbar: {
+    minHeight: 128,
+    alignItems: 'flex-start',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+    backgroundColor: 'white',
+    color: '#607D8B',
+  },
+  title: {
+    flexGrow: 1,
+    alignSelf: 'flex-end',
+  },
+}));
+
+export function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography className={classes.title} variant="h5" noWrap>
+            San Joaquin Valley Town Hall
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
